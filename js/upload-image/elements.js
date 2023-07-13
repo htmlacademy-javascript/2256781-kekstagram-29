4d1f61eeef84653cc5f262422d95c424465e9dc1
Dimaggio <zodiak-d@yandex.ru>
2023-07-13 23:41:34 +0300

    module9-task1

--- js/upload-image/elements.js
+++ js/upload-image/elements.js
@@ -1,5 +1,5 @@
-const uploadElement = document.querySelector('.img-upload__start input[type=file]');
-const previewElement = document.querySelector('.img-upload__preview img');
-const effectElement = document.querySelectorAll('.effects__preview');
+// const uploadElement = document.querySelector('.img-upload__start input[type=file]');
+// const previewElement = document.querySelector('.img-upload__preview img');
+// const effectElement = document.querySelectorAll('.effects__preview');
 
-export { uploadElement, previewElement, effectElement };
+// export { uploadElement, previewElement, effectElement };
