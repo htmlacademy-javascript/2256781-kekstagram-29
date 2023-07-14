a35396b813ce2c81a847cfd31f35a2584878f6f6
Dimaggio <zodiak-d@yandex.ru>
2023-07-15 01:02:42 +0300

    удалил лишнее

--- js/upload-image/elements.js
+++
@@ -1,5 +0,0 @@
-// const uploadElement = document.querySelector('.img-upload__start input[type=file]');
-// const previewElement = document.querySelector('.img-upload__preview img');
-// const effectElement = document.querySelectorAll('.effects__preview');
-
-// export { uploadElement, previewElement, effectElement };
