22c1d0d143f47f1b7630eedb0647baf8633bf9d0
Dimaggio <zodiak-d@yandex.ru>
2023-07-17 23:35:13 +0300

    filter, scale, constants, upload-form

--- js/upload-form/constants.js
+++ js/upload-form/constants.js
@@ -1,5 +1,76 @@
-const HASHTAG_CHARACTERS = /^#[a-zа-яё0-9]{1,19}$/i;
-const HASHTAG_COUNT = 5;
 const COMMENT_COUNT = 140;
 
-export { HASHTAG_CHARACTERS, HASHTAG_COUNT, COMMENT_COUNT };
+const HashTag = {
+  CHARACTERS: /^#[a-zа-яё0-9]{1,19}$/i,
+  COUNT: 5,
+};
+
+const Scale = {
+  RANGE: { min: 25, max: 100 },
+  DEFAULT: 100,
+  STEP: 25,
+};
+
+const EFFECTS = [
+  {
+    name: 'none',
+    style: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '',
+  },
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  },
+];
+
+const PrestineErrorText = {
+  INVALID_COUNT: `Количество хэштэгов должно быть не более ${HashTag.COUNT}`,
+  NOT_UNIQUE: 'Хэштэги повторяются',
+  INVALID_CONTENT: 'Вы использовали недопустимые символы в хэштэге',
+};
+
+const SubmitButtonText = {
+  DEFAULT: 'Опубликовать',
+  SUBMITTING: 'Отправляю...',
+};
+
+export { HashTag, Scale, COMMENT_COUNT, EFFECTS, PrestineErrorText, SubmitButtonText };
--- js/upload-form/elements.js
+++ js/upload-form/elements.js
@@ -1,17 +1,26 @@
-const formUploadElement = document.querySelector('.img-upload__form');
-const uploadFormCloseElement = document.querySelector('.img-upload__cancel');
-const fieldHashtagElement = document.querySelector('.text__hashtags');
-const commentFieldElement = document.querySelector('.text__description');
-const uploadSubmitElement = document.querySelector('.img-upload__submit');
-const sendSuccessElement = document.querySelector('#success').content.querySelector('.success');
-const sendErrorElement = document.querySelector('#error').content.querySelector('.error');
-
-export {
-  formUploadElement,
-  fieldHashtagElement,
-  commentFieldElement,
-  uploadSubmitElement,
-  sendSuccessElement,
-  sendErrorElement,
-  uploadFormCloseElement,
+const uploadFormElementList = {
+  formUploadElement: document.querySelector('.img-upload__form'),
+  uploadFormCloseElement: document.querySelector('.img-upload__cancel'),
+  fieldHashtagElement: document.querySelector('.text__hashtags'),
+  commentFieldElement: document.querySelector('.text__description'),
+  uploadSubmitElement: document.querySelector('.img-upload__submit'),
+  sendSuccessElement: document.querySelector('#success').content.querySelector('.success'),
+  sendErrorElement: document.querySelector('#error').content.querySelector('.error'),
 };
+
+const scaleElementList = {
+  picturePreviewElement: document.querySelector('.img-upload__preview img'),
+  smallerControlElement: document.querySelector('.scale__control--smaller'),
+  largerControlElement: document.querySelector('.scale__control--bigger'),
+  scaleControlElement: document.querySelector('.scale__control--value'),
+};
+
+const effectElementList = {
+  picturePreviewElement: document.querySelector('.img-upload__preview img'),
+  effectElement: document.querySelector('.effects__list'),
+  sliderElement: document.querySelector('.effect-level__slider'),
+  sliderContainerElement: document.querySelector('.img-upload__effect-level'),
+  effectLevelElement: document.querySelector('.effect-level__value'),
+};
+
+export { uploadFormElementList, scaleElementList, effectElementList };
