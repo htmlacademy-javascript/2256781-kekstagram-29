7360dd26d6f5d10f77d7b9f4e02efcd0c27a99aa
Dimaggio <zodiak-d@yandex.ru>
2023-07-22 00:23:46 +0300

    option for checking module12-task2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getData } from './api/api.js';
 import { render } from './home/home.js';
 import { showAlert } from './util.js';
+import './upload-image/upload-image.js';
 import './upload-form/upload-form.js';
 
 getData()
